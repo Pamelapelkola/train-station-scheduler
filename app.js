6f7cc980e31a8f899a7e4fa58ed300b55dd450c3
Pam Pelkola <pelkola.pam@gmail.com>
2018-03-27 18:58:15 -0500

    created bootstrap and added firebase to js file

---
+++ app.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    var config = {
+        apiKey: "AIzaSyAt4d3l20BNVVLD26aADUVWZGMzEo6p0EY",
+        authDomain: "train-scheduler-bdd78.firebaseapp.com",
+        databaseURL: "https://train-scheduler-bdd78.firebaseio.com",
+        projectId: "train-scheduler-bdd78",
+        storageBucket: "",
+        messagingSenderId: "1056839389134"
+      };
+      firebase.initializeApp(config);
+})   
